b00b4e1e5bdb39ad1b12dd95f4f8ae0df78d2533
Yini <ydxb7@mail.missouri.edu>
2019-10-09 17:48:15 -0700

    send eth

--- app/src/main/java/ai/tomorrow/mywallet/MainActivity.kt
+++ app/src/main/java/ai/tomorrow/mywallet/MainActivity.kt
@@ -1,27 +1,35 @@
 package ai.tomorrow.mywallet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import org.consenlabs.tokencore.wallet.KeystoreStorage
-import java.io.File
-import org.consenlabs.tokencore.wallet.model.Metadata.P2WPKH
-import android.provider.Telephony.Carriers.PASSWORD
-import org.consenlabs.tokencore.wallet.Identity
-import org.consenlabs.tokencore.wallet.Identity.createIdentity
-import org.consenlabs.tokencore.wallet.model.Network
-import org.consenlabs.tokencore.wallet.model.Metadata
-import android.provider.Telephony.Carriers.PASSWORD
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
-import okhttp3.OkHttpClient
-import org.consenlabs.tokencore.wallet.Identity.recoverIdentity
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import org.consenlabs.tokencore.wallet.Identity
+import org.consenlabs.tokencore.wallet.KeystoreStorage
+import org.consenlabs.tokencore.wallet.Wallet
 import org.consenlabs.tokencore.wallet.WalletManager
+import org.consenlabs.tokencore.wallet.model.ChainType
+import org.consenlabs.tokencore.wallet.model.Metadata
+import org.consenlabs.tokencore.wallet.model.Network
+import org.consenlabs.tokencore.wallet.transaction.EthereumTransaction
+import org.web3j.crypto.RawTransaction
+import org.web3j.crypto.TransactionEncoder
+import org.web3j.crypto.WalletUtils
 import org.web3j.protocol.Web3j
-import org.web3j.protocol.http.HttpService
 import org.web3j.protocol.core.DefaultBlockParameterName
-import org.web3j.protocol.core.methods.response.EthGetBalance
+import org.web3j.protocol.http.HttpService
 import org.web3j.utils.Convert
+import java.io.File
 import java.math.BigDecimal
+import java.math.BigInteger
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount
+import org.web3j.protocol.core.methods.response.EthSendTransaction
+import org.web3j.utils.Numeric
+import java.nio.file.Path
+import java.nio.file.Paths
 
 
 class MainActivity : AppCompatActivity(), KeystoreStorage {
@@ -31,10 +39,22 @@ class MainActivity : AppCompatActivity(), KeystoreStorage {
     private val SAMPLE_MNEMONIC1 = "world tired copper write maid monkey risk today husband hope grid inflict"
     private val SAMPLE_MNEMONIC2 = "mouse inject office junior repeat one tip actor drift love auto chase"
 
+    private val web3j = Web3j.build(HttpService("https://ropsten.infura.io/llyrtzQ3YhkdESt2Fzrk"))
+
+    private var uiHandler = Handler()
+    private lateinit var backgroundHandler: Handler
+    private lateinit var backgroundThread: HandlerThread
+
+    private val job = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WalletManager.storage = this
+        WalletManager.scanWallets()
 
 //        web3j = Web3j.build(HttpService("https://ropsten.infura.io/llyrtzQ3YhkdESt2Fzrk", httpClient, false))
 
@@ -42,8 +62,7 @@ class MainActivity : AppCompatActivity(), KeystoreStorage {
         sampleMnemonicBtn2.setOnClickListener { mnemonicEditText2.setText(SAMPLE_MNEMONIC2) }
 
 
-        WalletManager.storage = this
-        WalletManager.scanWallets()
+
 
         val identity1 = Identity.recoverIdentity(
             SAMPLE_MNEMONIC1,
@@ -82,18 +101,59 @@ class MainActivity : AppCompatActivity(), KeystoreStorage {
         Log.d(TAG, "Adress1: ${ethereumWallet1.address}")
         Log.d(TAG, "Adress2: ${ethereumWallet2.address}")
 
-
+        // get balance
         Log.d(TAG, "balance: ${getBalance(ethereumWallet1.address)}")
         Log.d(TAG, "balance: ${getBalance(ethereumWallet2.address)}")
 
+        // send
+        // keystore 文件路径
+        val directory = File(filesDir, "wallets")
+        val f = File(directory, "${ethereumWallet1.id}.json")
+        Log.d(TAG, "is file exist: ${f.exists()}")
+
+        send(ethereumWallet1, ethereumWallet2.address)
+
+
     }
+
+    private fun send(fromWallet: Wallet, toAddress: String){
+        uiScope.launch {
+
+            withContext(Dispatchers.IO){
+                val ethGetTransactionCount = web3j.ethGetTransactionCount(
+                    "0x${fromWallet.address}", DefaultBlockParameterName.LATEST
+                ).send()
+                val nonce = ethGetTransactionCount.transactionCount
+                val gasLimit = BigInteger.valueOf(21000L)
+//                val amount = BigInteger.valueOf(1)
+                val amount = Convert.toWei(BigDecimal(1L), Convert.Unit.ETHER).toBigInteger()
+                val gasPrice = web3j.ethGasPrice().send().gasPrice
+                Log.d(TAG, "gasPrice: $gasPrice")
+                Log.d(TAG, "nonce: $nonce")
+
+                val keystorePath = filesDir.absolutePath + "/wallets" + "/${fromWallet.id}.json"
+                val credentials = WalletUtils.loadCredentials("", keystorePath)
+
+                val rawTransaction = RawTransaction.createEtherTransaction(nonce, gasPrice, gasLimit, toAddress, amount)
+                val signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials)
+
+                val hexValue = Numeric.toHexString(signedMessage)
+                val ethSendTransaction = web3j.ethSendRawTransaction(hexValue).send()
+
+                Log.d(TAG, ethSendTransaction.toString())
+                Log.d(TAG, "transactionHash: ${ethSendTransaction.transactionHash}")
+
+            }
+        }
+    }
+
 
     private fun getBalance(address: String): BigDecimal{
         // connect to node
-        val web3 = Web3j.build(HttpService("https://ropsten.infura.io/llyrtzQ3YhkdESt2Fzrk"))  // defaults to http://localhost:8545/
+//        val web3 = Web3j.build(HttpService("https://ropsten.infura.io/llyrtzQ3YhkdESt2Fzrk"))  // defaults to http://localhost:8545/
 
         // send asynchronous requests to get balance
-        val ethGetBalance = web3
+        val ethGetBalance = web3j
             .ethGetBalance("0x$address", DefaultBlockParameterName.LATEST)
             .sendAsync()
             .get()
@@ -104,6 +164,7 @@ class MainActivity : AppCompatActivity(), KeystoreStorage {
 
 
     override fun getKeystoreDir(): File {
+//        Log.d(TAG, "getKeystoreDir = $filesDir")
         return this.filesDir
     }
 }
